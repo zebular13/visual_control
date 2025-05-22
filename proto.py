120d6c97c8be3d4a0c59d80a6b035edb0c111965
Lucas Keller <kellerjlucas@gmail.com>
2025-05-22 11:15:41 -0600

    Control feedback

--- proto.py
+++ proto.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import sys
 import time
 
@@ -14,6 +15,67 @@ CONTROL_CIRCLE_DEADZONE_R = 50
 CONTROL_CIRCLE_XY_CENTER = (int(CAMERA_WIDTH/4), int(CAMERA_HEIGHT/2))
 CONTROL_CIRCLE_Z_APERATURE_CENTER = (
     int(3*CAMERA_WIDTH/4), int(CAMERA_HEIGHT/2))
+
+
+@dataclass
+class HandData:
+    handedness: str
+    landmarks: list
+    center_perc: tuple
+
+    def __init__(self, handedness, landmarks):
+        self.handedness = handedness
+        self.landmarks = landmarks
+        x_avg = sum(lm.x for lm in landmarks) / len(landmarks)
+        y_avg = sum(lm.y for lm in landmarks) / len(landmarks)
+        z_avg = sum(lm.z for lm in landmarks) / len(landmarks)
+        self.center_perc = (x_avg, y_avg, z_avg)
+        print(f"center_perc: {self.center_perc}")
+
+
+def lerp(a, b, t):
+    return a(b-a)*t
+
+
+def draw_control_overlay(img, lh_data=None, rh_data=None):
+    cv2.circle(img, CONTROL_CIRCLE_XY_CENTER,
+               CONTROL_CIRCLE_DEADZONE_R, CV_DRAW_COLOR_PRIMARY, 2)
+
+    # TODO: bounds validation for both hands (ignore hands outside of certain ranges)
+    if lh_data:
+        # must normalize. if a hand has center of (0.5, 0.5) it means the hand is in the middle of the screen, but our xy control center is
+        # at (0.25, 0.5) so we need to scale the x value by 2
+        xy_ctl_x_pct_normalized = min((lh_data.center_perc[0] - 0.25) * 2, 1.0)
+        xy_ctl_x = int(
+            xy_ctl_x_pct_normalized * CONTROL_CIRCLE_DEADZONE_R) + CONTROL_CIRCLE_XY_CENTER[0]
+
+        xy_ctl_y_pct_normalized = min((lh_data.center_perc[1] - 0.5) * 2, 1.0)
+        xy_ctl_y = int(
+            xy_ctl_y_pct_normalized
+            * CONTROL_CIRCLE_DEADZONE_R) + CONTROL_CIRCLE_XY_CENTER[1]
+        cv2.circle(img, (xy_ctl_x, xy_ctl_y),
+                   4, CV_DRAW_COLOR_PRIMARY, cv2.FILLED)
+
+    cv2.circle(img, CONTROL_CIRCLE_Z_APERATURE_CENTER,
+               CONTROL_CIRCLE_DEADZONE_R, CV_DRAW_COLOR_PRIMARY, 2)
+    if rh_data:
+        z_ctl_pct_normalized = min(
+            (rh_data.center_perc[1] - 0.50) * 2, 1.0)
+        # y being the height in the image
+        z_ctl_y = int(
+            z_ctl_pct_normalized * CONTROL_CIRCLE_DEADZONE_R) + CONTROL_CIRCLE_Z_APERATURE_CENTER[1]
+
+        aperature_ctl_x_pct_normalized = min(
+            (rh_data.center_perc[0] - 0.75) * 2, 1.0)
+        # x being the width in the image
+        aperature_ctl_x = int(
+            aperature_ctl_x_pct_normalized * CONTROL_CIRCLE_DEADZONE_R) + CONTROL_CIRCLE_Z_APERATURE_CENTER[0]
+        print(f"apx, py: {aperature_ctl_x}, {z_ctl_y}")
+        cv2.circle(img, (aperature_ctl_x, z_ctl_y),
+                   4, CV_DRAW_COLOR_PRIMARY, cv2.FILLED)
+
+    cv2.line(img, (int(CAMERA_WIDTH/2), 0),
+             (int(CAMERA_WIDTH/2), CAMERA_HEIGHT), CV_DRAW_COLOR_PRIMARY, 1)
 
 
 def main():
@@ -38,16 +100,21 @@ def main():
         img_rgb_reduced = cv2.resize(img_rgb, (0, 0), fx=0.25, fy=0.25)
 
         hand_res = hands.process(img_rgb_reduced)
+        handcount = 0
+        lh_data, rh_data = None, None
         if hand_res.multi_hand_landmarks:
+            handcount = len(hand_res.multi_hand_landmarks)
             for (handedness, hand_landmarks) in zip(hand_res.multi_handedness, hand_res.multi_hand_landmarks):
-                print(handedness.classification[0].label)
+                if handedness.classification[0].label.lower() == 'left':
+                    lh_data = HandData(handedness, hand_landmarks.landmark)
+                else:
+                    rh_data = HandData(handedness, hand_landmarks.landmark)
+
                 for id, lm in enumerate(hand_landmarks.landmark):
-                    # print(id)
                     h, w, c = img.shape
                     cx, cy = int(lm.x*w), int(lm.y*h)
-                    cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
-                mpDraw.draw_landmarks(
-                    img, hand_landmarks, mpHands.HAND_CONNECTIONS)
+                    mpDraw.draw_landmarks(
+                        img, hand_landmarks, mpHands.HAND_CONNECTIONS)
 
         cTime = time.monotonic()
         fps = 1 / (cTime - pTime)
@@ -56,15 +123,7 @@ def main():
         cv2.putText(img, f'FPS:{int(fps)}', (20, 70),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-        # control overlay
-        cv2.circle(img, CONTROL_CIRCLE_XY_CENTER,
-                   CONTROL_CIRCLE_DEADZONE_R, CV_DRAW_COLOR_PRIMARY, 2)
-        cv2.circle(img, CONTROL_CIRCLE_Z_APERATURE_CENTER,
-                   CONTROL_CIRCLE_DEADZONE_R, CV_DRAW_COLOR_PRIMARY, 2)
-
-        cv2.line(img, (int(CAMERA_WIDTH/2), 0),
-                 (int(CAMERA_WIDTH/2), CAMERA_HEIGHT), CV_DRAW_COLOR_PRIMARY, 1)
-
+        draw_control_overlay(img, lh_data, rh_data)
         cv2.imshow("Test", img)
         cv2.waitKey(1)
 
